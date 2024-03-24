ba87d335c55b1dd06bc1ea5acd330e11d616ec98
Divyanshu chouhan <100332571+DivyanshuChouhan@users.noreply.github.com>
2024-03-24 09:06:01 +0000

    commit config done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,10 @@
+const accountId = 144553 //cannot be changed 
+let accountEmail ="div.@gmail.com"
+var accountPassword =  "12545"//not to use var issue in block scope and functional scope
+accountCity = "jaipur"//can use but we should not use
+// accountId = 2 not allowed because it is stored in const
+console.log(accountId);
+accountEmail = "dcdc@gmail.com"
+accountPassword ="21212"
+accountCity ="Ujjain"
+ console.table([accountId,accountEmail,accountPassword,accountCity])// to print all in tabluar for 
