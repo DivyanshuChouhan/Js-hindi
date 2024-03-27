2ca92c44f12f14026d9d42231f1756c5cf396e5d
Divyanshu chouhan <100332571+DivyanshuChouhan@users.noreply.github.com>
2024-03-27 11:26:05 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,15 +1,15 @@
 let score = 22
-console.log(typeof score);//output is number
-console.log(typeof(score));//output is number
+///console.log(typeof score);//output is number
+///console.log(typeof(score));//output is number
 
 let marks = "55abc"
-console.log(typeof marks);//output is string
+///console.log(typeof marks);//output is string
 
 
 let valueInNumber = Number(marks);
-console.log(typeof valueInNumber);//Output is number
+///console.log(typeof valueInNumber);//Output is number
 
-console.log(valueInNumber);//Output is NaN(Not a number) because as we can see value of marks is "55abc"
+///console.log(valueInNumber);//Output is NaN(Not a number) because as we can see value of marks is "55abc"
 
 //If we convert "33" it become 33 
 //If we convert "55abc" it become NaN
@@ -18,8 +18,35 @@ console.log(valueInNumber);//Output is NaN(Not a number) because as we can see v
 //Other conversions
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);//Output is true
+///console.log(booleanIsLoggedIn);//Output is true
 
 //1=> true ; 0 =>false
 //""(empty string)=> false
 //"divyanshu"=>true
+
+//**************OPERATIONS**********
+
+let value = 3
+let negValue= -value
+//console.log(negValue); //output = -3
+
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);//2 to the power 2
+// console.log(2%3);
+
+let str1 = "divyanshu"
+let str2 = " chouhan"
+let str3 = str1 + str2
+//console.log(str3)
+
+// console.log(1 + 2)
+// console.log("1"+ 2)
+// console.log(1+"2")
+// console.log("1"+2+2)//output 122
+// console.log(1+2+"2")//output 32
+
