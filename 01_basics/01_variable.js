f75e3c94ec3085e2167a046c88b071df58cd4bb8
Divyanshu chouhan <100332571+DivyanshuChouhan@users.noreply.github.com>
2024-04-03 06:04:29 +0000

    variable

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,8 +1,9 @@
 const accountId = 144553 //cannot be changed 
+// accountId = 2 not allowed because it is stored in const
 let accountEmail ="div.@gmail.com"
 var accountPassword =  "12545"//not to use var issue in block scope and functional scope
 accountCity = "jaipur"//can use but we should not use
-// accountId = 2 not allowed because it is stored in const
+
 console.log(accountId);
 accountEmail = "dcdc@gmail.com"
 accountPassword ="21212"
